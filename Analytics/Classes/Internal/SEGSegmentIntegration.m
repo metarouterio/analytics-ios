f67556fb0a35d8b0905bb2fda9d53bbac6726da6
Alois Barreras <alois@vicino.io>
2016-09-07 20:58:05 -0400

    updated urls for astronomer

--- Analytics/Classes/Internal/SEGSegmentIntegration.m
+++ Analytics/Classes/Internal/SEGSegmentIntegration.m
@@ -97,7 +97,7 @@ static BOOL GetAdTrackingEnabled()
         self.analytics = analytics;
         self.configuration = analytics.configuration;
         self.httpClient = analytics.httpClient;
-        self.apiURL = [NSURL URLWithString:@"https://api.segment.io/v1/import"];
+        self.apiURL = [NSURL URLWithString:@"https://api.astronomer.io/v1/import"];
         self.userId = [self getUserId];
         self.bluetooth = [[SEGBluetooth alloc] init];
         self.reachability = [SEGReachability reachabilityWithHostname:@"google.com"];
